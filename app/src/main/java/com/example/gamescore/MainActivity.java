d64aa4c51d641be78d16f2e01634f6649b7d67a7
datoona <davit.arzumanyan92@gmail.com>
2023-06-06 12:09:44 +0400

    add life cycle

--- app/src/main/java/com/example/gamescore/MainActivity.java
+++ app/src/main/java/com/example/gamescore/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,42 +13,87 @@ public class MainActivity extends AppCompatActivity {
     private int score1 = 0;
     private int score2 = 0;
 
+    private TextView textScore1;
+    private TextView textScore2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "onCreate");
+
+        textScore1 = findViewById(R.id.textViewScore1);
+        textScore2 = findViewById(R.id.textViewScore2);
 
         if (savedInstanceState != null) {
             score1 = savedInstanceState.getInt("score1");
             score2 = savedInstanceState.getInt("score2");
         }
 
-        TextView textScore1 = findViewById(R.id.textViewScore1);
-        TextView textScore2 = findViewById(R.id.textViewScore2);
 
-        textScore1.setText(String.valueOf(score1));
-        textScore2.setText(String.valueOf(score1));
+        updateScore1();
+        updateScore2();
+
 
         textScore1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textScore1.setText(String.valueOf(++score1));
+                Log.d("MainActivity", "Click on Team 1");
+                score1++;
+                updateScore1();
             }
         });
 
-        textScore2.setOnClickListener(v -> textScore2.setText(String.valueOf(++score2)));
+        textScore2.setOnClickListener(v -> {
+            Log.d("MainActivity", "Click on Team 2");
+            score1++;
+            updateScore2();
+        });
     }
 
+    private void updateScore1() {
+        textScore1.setText(String.valueOf(score1));
+    }
+
+    private void updateScore2() {
+        textScore2.setText(String.valueOf(score2));
+    }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
+        Log.d("MainActivity", "onSaveInstanceState");
         outState.putInt("score1", score1);
         outState.putInt("score2", score2);
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
     }
 }
